cecce2d50ba0ab6d5043e36fa2a2326450f24993
aditya2118110 <2118110@scholar.itn.ac.id>
2023-11-21 08:47:45 +0700

    Movie

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://api.themoviedb.org/3/';
+const posterImageUrl = 'https://image.tmdb.org/t/p/';
+const apiKey = 'd6e57f106c8374cd4e2230af42dc2f90';
+
+export {baseUrl, posterImageUrl, apiKey};
